77635112c77958452105a1668f0b3a7ed2605da5
elizabeth-oh <91884455+elizabeth-oh@users.noreply.github.com>
2021-11-04 11:57:21 +0100

    Added timestamp

--- src/app.js
+++ src/app.js
@@ -1,18 +1,37 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()]
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+
+    return `${day} ${hours}:${minutes}`
+}
+
 function displayWeather(response) {
     let cityElement = document.querySelector("#city");
     let currentTempElement = document.querySelector("#currentTemp");
     let currentWeatherDescElement = document.querySelector("#currentWeatherDesc");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let currentTimeElement = document.querySelector("#currentTime");
+
     cityElement.innerHTML = response.data.name;
     currentTempElement.innerHTML = Math.round(response.data.main.temp);
     currentWeatherDescElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    currentTimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "2465cf67c79b581f012f9a417fbb141d"
-let city = "New York"
+let city = "Paris"
 let units = "metric"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
 
